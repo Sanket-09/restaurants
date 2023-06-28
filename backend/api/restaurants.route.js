7115c534adc02fed571d822d0b8f67c08500006e
Sanket-09 <f20200356@hyderabad.bits-pilani.ac.in>
2023-06-28 20:02:31 +0530

    post review error

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,7 +1,16 @@
 import express from 'express'
 import RestaurantsCtrl from './restaurants.controller.js'
+import ReviewsCtrl from './reviews.controller.js'
 
 const router = express.Router() //get access to the express router
 router.route('/').get(RestaurantsCtrl.apiGetRestaurants) //route comes from second import
+
+//adding routes for deleting,updating and posting reviews
+
+router
+  .route('/review')
+  .post(ReviewsCtrl.apiPostReview)
+  .put(ReviewsCtrl.apiUpdateReview)
+  .delete(ReviewsCtrl.apiDeleteReview)
 
 export default router
